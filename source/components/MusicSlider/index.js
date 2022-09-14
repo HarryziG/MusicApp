454d632b62ec9faa0814d065a67f47fd9488bb72
HarryziG <viniciuslatini2001@gmail.com>
2022-09-14 14:29:34 -0300

    Alterando o tempo da música com Slider

--- source/components/MusicSlider/index.js
+++ source/components/MusicSlider/index.js
@@ -4,29 +4,28 @@ import Slider from '@react-native-community/slider';
 
 export function MusicSlider({soundDuration, sound}) {
 
-    const [actualValueSound, setActualValueSound] = useState(0)
+  const [actualValueSound, setActualValueSound] = useState(0)
 
-    useEffect(() => {
-		if (sound) {
-			sound.setOnPlaybackStatusUpdate((data) => {
-                setActualValueSound(data.positionMillis)
-            })
-		}
-	}, [sound]);
+  useEffect(() => {
+  if (sound) {
+    sound.setOnPlaybackStatusUpdate((data) => {
+      setActualValueSound(data.positionMillis)
+    })
+  }
+}, [sound]);
 
-    return (
-        <Slider
-            style={{ width: 350, height: 40 }}
-            minimumValue={0}
-            maximumValue={soundDuration || 0}
-            minimumTrackTintColor="#0500FF"
-            thumbTintColor="#FFFFFF"
-            maximumTrackTintColor="#C4C4C4"
-            value={actualValueSound || 0}
-            // onSlidingComplete={e => {
-            //     setActualValueSound(e)
-            // }}
-        /> 
-    )
+  return (
+    <Slider
+      style={{ width: 350, height: 40 }}
+      minimumValue={0}
+      maximumValue={soundDuration || 0}
+      minimumTrackTintColor="#0500FF"
+      thumbTintColor="#FFFFFF"
+      maximumTrackTintColor="#C4C4C4"
+      value={actualValueSound || 0}
+      onSlidingComplete={e => {
+        sound.setPositionAsync(e);
+      }}
+    />
+  )
 }
-
